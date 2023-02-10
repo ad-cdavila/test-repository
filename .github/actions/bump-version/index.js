f53fd2c53a3cdca0755ad02af84733a0f5d38698
Cristopher Dávila <cristopher.davila@crifa.com>
2023-02-10 12:55:00 -0500

    test

---
+++ .github/actions/bump-version/index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const semver = require('semver');
+const gitLatestSemverTag = require('git-latest-semver-tag');
+
+const SUPPORTED_RELEASES = ['major', 'minor', 'patch'];
+
+const getLatestTag = () => new Promise((resolve, reject) => {
+  gitLatestSemverTag((err, tag) => {
+    if (err) reject(err);
+
+    resolve(tag);
+  });
+});
+
+(async () => {
+  try {
+    let latestTag = await getLatestTag();
+
+    if (!latestTag) {
+      latestTag = '0.0.0';
+    } else {
+      latestTag = semver.valid(latestTag);
+    }
+
+    const increment = core.getInput('increment');
+
+    if (!SUPPORTED_RELEASES.includes(increment)) {
+      throw new Error(`Increment ${increment} not supported`);
+    }
+
+    const bumpVersion = semver.inc(latestTag, increment);
+    core.setOutput('bump-version', bumpVersion);
+
+    console.log(`Current version: ${latestTag}`);
+    console.log(`Bump version: ${bumpVersion}`);
+  } catch (error) {
+    core.setFailed(error);
+  }
+})()
