6a3ed67a1abec3cc66a63bf0bc49918e9c06e26e
Cristopher Dávila <cristopher.davila@crifa.com>
2023-02-13 23:59:17 -0500

    feat(android): testing message

---
+++ .release-it.js
@@ -0,0 +1,34 @@
+module.exports = {
+  plugins: {
+    git: {
+      tag: false,
+      commitMessage: "chore(release): v${version}"
+    },
+    "@release-it/bumper": {
+      in: {
+        "file": "app.json",
+        "path": "expo.version"
+      },
+      out: {
+        "file": "app.json",
+        "path": "expo.version"
+      }
+    },
+    "@release-it/conventional-changelog": {
+      infile: "CHANGELOG.md",
+      "preset": {
+        "name": "conventionalcommits",
+        "types": [
+          {
+            "type": "feat",
+            "section": ":rocket: Features"
+          },
+          {
+            "type": "fix",
+            "section": ":bug: Bug Fixes"
+          }
+        ]
+      }
+    }
+  }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+
+
+### [3.1.7](https://github.com/ad-cdavila/test-repository/compare/test_0.0.0...3.1.7) (2023-02-14)
+
+
+### :bug: Bug Fixes
+
+* remove unnecessary ([6831191](https://github.com/ad-cdavila/test-repository/commit/683119148e663835297b74a9ca6be0126c3e4afd))
